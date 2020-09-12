13390f81d98db3723b46d4e138d2d934c9956be0
qw673068306 <673068306@qq.com>
2020-09-12 17:03:24 +0800

    增加测试报告编码

--- Chapter12/src/main/java/com/course/config/ExtentTestNGIReporterListener.java
+++ Chapter12/src/main/java/com/course/config/ExtentTestNGIReporterListener.java
@@ -117,6 +117,7 @@ public class ExtentTestNGIReporterListener implements IReporter {
         htmlReporter.config().setChartVisibilityOnOpen(true);
         htmlReporter.config().setTestViewChartLocation(ChartLocation.TOP);
         htmlReporter.config().setTheme(Theme.STANDARD);
+        htmlReporter.config().setEncoding("gbk");
         htmlReporter.config().setCSS(".node.level-1  ul{ display:none;} .node.level-1.active ul{display:block;}");
         extent = new ExtentReports();
         extent.attachReporter(htmlReporter);
